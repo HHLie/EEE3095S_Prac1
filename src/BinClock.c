c2b9f33b5c13938a13424c1c8920a0e8c289e25f
LXXHSI007 <lxxhsi007@myuct.ac.za>
2021-08-18 13:18:12 +0200

    BUttons????

--- src/BinClock.c
+++ src/BinClock.c
@@ -72,7 +72,8 @@ void initGPIO(void){
 
 	//Attach interrupts to Buttons
 	//Write your logic here
-
+	wiringPiISR(BTNS[0],INT_EDGE_FALLING,hourInc);
+	wiringPiISR(BTNS[1],INT_EDGE_FALLING,minInc);
 
 
 	printf("BTNS done\n");
